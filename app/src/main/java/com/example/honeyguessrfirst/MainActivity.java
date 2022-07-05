23979685464a62b95d7cf8e43c10dffd1ba97e32
Gabriel Alfonso Mapa <gabrielalfonsomapa@gmail.com>
2022-07-05 22:44:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/honeyguessrfirst/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.honeyguessrfirst;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.mainLayout);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
+    }
+}
